58a098f20030bbe46b95f8d34b29e6b5869e336f
farhan-navas <farhanmnavas@gmail.com>
2024-05-14 23:20:36 +0800

    changed to postgresql

--- src/server/twilio.ts
+++ src/server/twilio.ts
@@ -1,17 +1,17 @@
-const twilio = require("twilio");
-require("dotenv").config();
+// const twilio = require("twilio");
+// require("dotenv").config();
 
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
+// const accountSid = process.env.TWILIO_ACCOUNT_SID;
+// const authToken = process.env.TWILIO_AUTH_TOKEN;
 
-const client = twilio(accountSid, authToken);
+// const client = twilio(accountSid, authToken);
 
-export const sendSMS = (phoneNumber: string) => {
-  client.messages
-    .create({
-      from: process.env.TWILIO_PHONE_NUMBER,
-      to: phoneNumber,
-      body: "It's your turn to bid for a room!",
-    })
-    .then((message: any) => console.log(message.sid));
-};
+// export const sendSMS = (phoneNumber: string) => {
+//   client.messages
+//     .create({
+//       from: process.env.TWILIO_PHONE_NUMBER,
+//       to: phoneNumber,
+//       body: "It's your turn to bid for a room!",
+//     })
+//     .then((message: any) => console.log(message.sid));
+// };
