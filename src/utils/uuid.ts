339199e2634037d0082fc97bb0d324928ff78596
farhan-navas <farhanmnavas@gmail.com>
2024-05-15 00:24:23 +0800

    working

---
+++ src/utils/uuid.ts
@@ -0,0 +1,8 @@
+import { v5 as uuidv5 } from "uuid";
+
+export function generateUUID(str1: string, str2: string): string {
+  const hash = `${str1}${str2}`;
+  const namespace = "1b671a64-40d5-491e-99b0-da01ff1f3341"; // Example UUID namespace
+
+  return uuidv5(hash, namespace);
+}
