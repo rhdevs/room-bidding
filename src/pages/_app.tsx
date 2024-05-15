05006cce778342e45a5e5ac580b95f1fbb380f6c
farhan-navas <farhanmnavas@gmail.com>
2024-05-16 02:32:01 +0800

    naive authentication

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,16 @@ import { api } from "~/utils/api";
 import { Toaster } from "react-hot-toast";
 import "~/styles/globals.css";
 import Navbar from "~/components/Navbar";
+import { useSearchParams } from "next/navigation";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
+  const URLparams = useSearchParams();
+
+  const userhash = URLparams.get("id") ?? "User not Signed In";
   return (
     <>
       <Navbar />
+      {userhash}
       <Component {...pageProps} />
       <Toaster />
     </>
