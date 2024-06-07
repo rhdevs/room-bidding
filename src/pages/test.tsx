c6c0047c4f2cc290a775c20845762f0481b53237
chuahziyang <czyang2002@gmail.com>
2024-06-07 11:28:36 +0800

    added email sending

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,16 +1,18 @@
 import React from "react";
+import { Button } from "~/components/ui/button";
+import { api } from "~/utils/api";
 
 const QueuePage: React.FC = () => {
-  return <div>Loading...</div>;
   // const { data: rooms, isSuccess } = api.email.test.useQuery("test");
+  const mutation = api.email.sendEmailtoUser.useMutation();
   // if (!isSuccess) {
   //   return <div>Loading...</div>;
   // }
-  // return (
-  //   <div className="flex h-screen flex-col items-center justify-center">
-  //     {JSON.stringify(rooms)}
-  //   </div>
-  // );
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <Button onClick={() => mutation.mutate("e1123255")}>Test</Button>
+    </div>
+  );
 };
 
 export default QueuePage;
--- src/server/api/routers/emails.ts
+++ src/server/api/routers/emails.ts
@@ -1,24 +1,39 @@
 import { Resend } from "resend";
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { hashandSlice } from "~/utils/uuid";
 import { env } from "./../../../env";
 
 const resend = new Resend(env.RESEND_API_KEY);
 
-export const emailRouter = createTRPCRouter({
-  test: publicProcedure.input(z.string()).query(() => {
-    return resend.emails.send({
-      from: "Acme <onboarding@resend.dev>",
-      to: ["czyang2002@gmail.com"],
-      subject: "Hello World",
-      html: "<strong>It works!</strong>",
-    });
-  }),
-  sendAll: publicProcedure.query(async ({ ctx }) => {
-    const users = await ctx.db.user.findMany();
+const initialMessage = async (id: string) => {
+  return await resend.emails.create({
+    from: "Acme <onboarding@resend.dev>",
+    // to: [`${id}@u.nus.edu`],
+    to: ["czyang2002@gmail.com"],
+    subject: "Hello World",
+    html: `<strong>
+            This is your code for Room Bidding:
+            <br />
+            <a href={https://localhost:3000/?id=${hashandSlice(id)}}>https://localhost:3000/?id=${hashandSlice(id)}</a>
+          </strong>`,
+  });
+};
 
-    const userdata = users.map((user) => {});
-  }),
+export const emailRouter = createTRPCRouter({
+  sendEmailtoUser: publicProcedure
+    .input(z.string())
+    .mutation(async ({ input }) => {
+      const result = await initialMessage(input);
+      console.log(result);
+      return result;
+    }),
+
+  // sendAll: publicProcedure.query(async ({ ctx }) => {
+  //   const users = await ctx.db.user.findMany();
+
+  //   const userdata = users.map((user) => {});
+  // }),
 });
 
 // hello: publicProcedure
