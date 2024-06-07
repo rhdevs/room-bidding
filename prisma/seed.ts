52287cfc07bcff39500fb1001a86e4287867e0ae
chuahziyang <czyang2002@gmail.com>
2024-06-07 12:20:55 +0800

    added double room seeding

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -8,32 +8,44 @@ const rooms = [
     block: "B2",
     unit: 101,
     gender: "Male",
+    index: 1
+  },
+  {
+    block: "B2",
+    unit: 101,
+    gender: "Male",
+    index: 2
   },
   {
     block: "B2",
     unit: 102,
     gender: "Male",
+    index: 1
   },
   {
     block: "B2",
     unit: 103,
     gender: "Male",
+    index: 1
   },
   {
     block: "B2",
     unit: 104,
     gender: "Male",
+    index: 1
   },
   {
     block: "B2",
     unit: 105,
     gender: "Male",
+    index: 1
   },
 ] as const;
 
+
 const processedRooms = rooms.map((x) => {
   return {
-    id: generateUUID(x.block, x.unit),
+    id: generateUUID(x.block, x.unit,x.index),
     block: x.block,
     unit: x.unit,
     gender: x.gender,
@@ -43,6 +55,8 @@ const processedRooms = rooms.map((x) => {
 console.log(processedRooms);
 
 async function seedRooms() {
+  await prisma.user.deleteMany();
+  await prisma.room.deleteMany();
   await prisma.room.createMany({
     data: processedRooms,
   });
