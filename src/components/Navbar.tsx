662454541cbe46fd7c59d71ae3f0507f65f72673
chuahziyang <czyang2002@gmail.com>
2024-06-14 00:45:29 +0800

    remove search bar for now

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,7 @@
 import { CalendarIcon } from "@radix-ui/react-icons";
-import { Menu, Package2, Search } from "lucide-react";
+import { Menu, Package2 } from "lucide-react";
 import Link from "next/link";
 import { Button } from "~/components/ui/button";
-import { Input } from "~/components/ui/input";
 import { Sheet, SheetContent, SheetTrigger } from "~/components/ui/sheet";
 import { useUser } from "~/hooks/useUser";
 import { LinkWithQP } from "./ui/link";
@@ -122,14 +121,14 @@ const Navbar: React.FC = () => {
       </Sheet>
       <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
         <form className="ml-auto flex-1 sm:flex-initial">
-          <div className="relative">
+          {/* <div className="relative">
             <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
             <Input
               type="search"
               placeholder="Search products..."
               className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
             />
-          </div>
+          </div> */}
         </form>
 
         <div className="text-sm text-muted-foreground">
