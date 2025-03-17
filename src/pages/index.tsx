73fb0e379d86af80b0d4aa9884ae0d2d26804ccb
chuahziyang <czyang2002@gmail.com>
2025-03-18 03:57:59 +0800

    asd

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,7 +23,7 @@ export default function Home() {
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-muted-foreground/10 p-4 text-primary hover:bg-muted-foreground/20"
-              href="/room"
+              href="/rooms"
               target="_blank"
             >
               <h3 className="text-2xl font-bold">View Rooms→</h3>
